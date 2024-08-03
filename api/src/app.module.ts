a604d8a16c5926f69443ea064038b1e32cf7a56c
gabbwhxz <vnvsgabe@gmail.com>
2024-08-03 18:36:58 -0300

    adicionado module do cliente no app

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 
+import { ClienteModule } from './cliente/cliente.module';
+
 @Module({
-  imports: [],
+  imports: [ClienteModule],
   controllers: [],
   providers: [],
 })
