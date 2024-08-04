5acc2350a9ebf3ffa1d51245b9e186ab9a572d0e
gabbwhxz <vnvsgabe@gmail.com>
2024-08-03 21:59:21 -0300

    adicionado cores ao tema

--- app/src/global/theme.ts
+++ app/src/global/theme.ts
@@ -1,5 +1,12 @@
 const theme = {
-   colors: {},
+   colors: {
+      primary: '#0d98ba',
+      hoverPrimary: '#18839e',
+      white: '#f1faee',
+      black: '#161616',
+      cancel: '#e04646',
+      hoverCancel: '#ab3333',
+   },
 }
 
 export default theme
