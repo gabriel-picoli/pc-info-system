2a6ecb639cc9b87d8ab83cd964e3ab40abbaae76
gabbwhxz <vnvsgabe@gmail.com>
2024-08-07 19:49:31 -0300

    importado servico para dentro do app.module

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 
 import { ClienteModule } from './cliente/cliente.module';
+import { ServicoService } from './servico/servico.service';
+import { ServicoModule } from './servico/servico.module';
 
 @Module({
-  imports: [ClienteModule],
+  imports: [ClienteModule, ServicoModule],
   controllers: [],
-  providers: [],
+  providers: [ServicoService],
 })
 export class AppModule {}
