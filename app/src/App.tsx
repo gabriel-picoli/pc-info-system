c17559f7e82787dbb1525d8d14cfcaa14e2679f3
gabbwhxz <vnvsgabe@gmail.com>
2024-08-08 19:42:54 -0300

    adicionado toast-container ao app

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -3,6 +3,9 @@ import { Routes, Route } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
 import theme from './global/theme'
 
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+
 import GlobalStyle from './global/GlobalStyle'
 
 import Home from './pages/HomePage'
@@ -19,6 +22,7 @@ export default function App() {
                <Route path="/servicos" element={<Servicos />} />
             </Routes>
 
+            <ToastContainer position="bottom-left" autoClose={3000} newestOnTop={false} closeOnClick pauseOnHover />
             <GlobalStyle />
          </ThemeProvider>
       </>
